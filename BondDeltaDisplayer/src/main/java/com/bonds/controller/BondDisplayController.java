ff6e199d2d55b12a09937c591580cb1fdd5edc84
nmaffa <nmaffattone@sapient.com>
2015-04-30 17:43:29 -0400

    Table printed out of day 2 Price changes (but not nice looking)

--- BondDeltaDisplayer/src/main/java/com/bonds/controller/BondDisplayController.java
+++ BondDeltaDisplayer/src/main/java/com/bonds/controller/BondDisplayController.java
@@ -1,6 +1,10 @@
 package com.bonds.controller;
 
+import java.math.BigDecimal;
 import java.util.Queue;
+
+import com.bonds.conversionmethods.ConversionMethods;
+import com.bonds.model.Bond;
 
 public class BondDisplayController {
 	
@@ -12,12 +16,40 @@ public class BondDisplayController {
 	
 	public void printDay2Bonds(){
 		
-		Queue bondQueue = bondCompare.compareBonds();
+		Queue<Bond> bondQueue = bondCompare.compareBonds();
 		
-		while(!bondQueue.isEmpty()){
-			System.out.println(bondQueue.poll().toString());
+		final Object[][] table = new String[bondQueue.size() + 1][4];
+		
+		table[0][0] = "Bond Issue   |";
+		table[0][1] = "Fractional Price|";
+		table[0][2] = "Change    |";
+		table[0][3] = "Decimal Price";
+		
+		int index = 1;
+		
+		while (!bondQueue.isEmpty()){
+			Bond bond = bondQueue.poll();
+			table[index][0] = ConversionMethods.couponRateAndDateToIssue(bond);
+			table[index][1] = ConversionMethods.decimalPriceToBondFormatPrice(bond);
+			table[index][2] = changeTo32(bond);
+			table[index][3] = "$" + bond.getPrice().toString();
+			index++;
 		}
 		
+
+		for (final Object[] row : table) {
+		    System.out.format("%-15s%-15s%-15s%-15s\n", row);
+		    //System.out.println(row);
+		}
+		
+	}
+	
+	public String changeTo32(Bond bond){
+		BigDecimal bd = bond.getChange().multiply(new BigDecimal(32));
+		bd.setScale(1);
+		bd.stripTrailingZeros();
+		
+		return bd.toPlainString();
 	}
 
 }
